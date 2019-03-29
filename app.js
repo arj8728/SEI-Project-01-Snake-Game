334fc1953d6e39f267fc3d9dad8274ad2a4cd418
Arjun Modi <mrarjunmodi@gmail.com>
2019-03-29 10:13:47 +0000

    created grid

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 document.addEventListener('DOMContentLoaded', () =>{
 
-  console.log('Hello')
+  const grid = document.querySelector('.grid')
+  const width = 18
+  const squares = []
+
+  for(let i = 0; i < width * width; i++) {
+    const square = document.createElement('DIV')
+    squares.push(square)
+    grid.appendChild(square)
+  }
+
 })
